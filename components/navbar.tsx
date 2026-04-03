14e33101384e964246b98c74f3c7dba1e6700d61
Darrin Holtz <darrinholtz@gmail.com>
2026-04-03 16:43:09 +0000

    feat: initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export function Navbar() {
+    return(
+        <nav className="w-full py-5 flex items-center justify-between">
+            <div className="flex items-center gap-8">
+                <Link href="/">
+                    <h1 className="text-3xl font-bold">
+                        Darrin<span className="text-blue-500">Holtz</span>
+                    </h1>
+                </Link>
+                <div className="flex items-center gap-2">
+                    <Link href="/blog">Blog</Link>
+                    <Link href="/blog/create">Create</Link>
+                </div>                
+            </div>
+            <div className="flex items-center gap-2">
+                <Link href="/auth/sigm-up">Sign up</Link>
+                <Link href="/auth/login">Login</Link>
+            </div>
+        </nav>
+    )
+}
