b80dd2335fd5c911643e15dfaa49af4ddd53a081
Darrin Holtz <darrinholtz@gmail.com>
2026-04-07 13:22:36 +0000

    started optimizing blog page

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,13 +1,12 @@
-'use client';
-
-import { Card, CardContent } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
+import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function BlogPage() {
-    const data = useQuery(api.posts.getPosts);
   return (
     <div className="py-12">
       <div className="text-center pb-12">
@@ -18,20 +17,50 @@ export default function BlogPage() {
           Insights, thoughts and trends from our team.
         </p>
       </div>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {data?.map((post) => (
-          <Card key={post._id}>
-            <div className="relative h-48 w-full overflow-hidden">
-              <Image src="https://images.unsplash.com/photo-1773332611550-9f5a465b4e42?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D" alt="Image" fill />
-            </div>
-            <CardContent>
-              <Link href={`/blog/${post._id}`}>
-                <h1 className="text-2xl font-bold hover: text-primary">{post.title}</h1>
-              </Link>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoadBlogList />
+      </Suspense>
+    </div>
+  );
+}
+
+async function LoadBlogList() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const data = await fetchQuery(api.posts.getPosts);
+
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {data?.map((post) => (
+        <Card key={post._id} className="pt-0">
+          <div className="relative h-48 w-full overflow-hidden">
+            <Image
+              src="https://images.unsplash.com/photo-1761019646782-4bc46ba43fe9?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="image"
+              fill
+              className="rounded-t-lg object-cover"
+            />
+          </div>
+
+          <CardContent>
+            <Link href={`/blog/${post._id}`}>
+              <h1 className="text-2xl font-bold hover:text-primary">
+                {post.title}
+              </h1>
+            </Link>
+            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+          </CardContent>
+          <CardFooter>
+            <Link
+              className={buttonVariants({
+                className: "w-full",
+              })}
+              href={`/blog/${post._id}`}
+            >
+              Read more
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 }
